368ebcf05a2cd2513878dc2181c27357094eefdf
busracanakci <36260763+busracanakci@users.noreply.github.com>
2022-04-01 11:18:17 +0300

    javascript3 html sayfasındaki form ve js kodları düzenlendi

--- src/script3.js
+++ src/script3.js
@@ -7,21 +7,8 @@ function btnclick() {
     var soyad=document.getElementById('soyisim').value;
     var yas=document.getElementById('yas').value;
     var mail=document.getElementById('email').value;
-    document.getElementById('f1').innerHTML = ad;
-    document.getElementById('f2').innerHTML = soyad;
-    document.getElementById('f3').innerHTML = yas;
-    document.getElementById('f4').innerHTML = mail;
-    if(ad == ""){
-        alert("Ad alanı zorunludur!");
-    }
-    if(soyad==""){
-        alert("Soyad alanı zorunludur!");
-    }
-    if(mail== "")
-    {
-        alert("Mail adresi zorunludur!");
-    }
-
+    document.getElementById('form1').innerHTML = ad +" "+soyad +" "+ yas+" "+ mail;
+ 
 }
 
 
