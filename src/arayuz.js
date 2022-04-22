b38db3455fa79580e6068adb2deb81e01042d9b4
busracanakci <36260763+busracanakci@users.noreply.github.com>
2022-04-22 16:59:24 +0300

    mobile first arayüz çalışıldı

---
+++ src/arayuz.js
@@ -0,0 +1,10 @@
+function arttir() {
+    var goster=document.getElementById("goster");
+    goster.value=Number(goster.value)+1;
+}
+
+function sifirla() {
+    document.getElementById("goster").value = 0;
+}
+
+
